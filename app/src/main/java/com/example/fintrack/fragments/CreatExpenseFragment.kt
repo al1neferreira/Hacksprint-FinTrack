5448038f0735d4a4caef7334e06bec2e193e1047
Aline Ferreira <alinfst@gmail.com>
2024-05-16 21:00:04 -0300

    add fragment UI

---
+++ app/src/main/java/com/example/fintrack/fragments/CreatExpenseFragment.kt
@@ -0,0 +1,56 @@
+package com.example.fintrack.fragments
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.core.view.MenuProvider
+import com.example.fintrack.R
+import com.example.fintrack.databinding.FragmentCreatExpenseBinding
+
+class CreatExpenseFragment : Fragment(R.layout.fragment_creat_expense), MenuProvider {
+
+    private var creatExpenseBinding: FragmentCreatExpenseBinding? = null
+    private val binding get() = creatExpenseBinding!!
+
+    //inserir viewModel
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        creatExpenseBinding = FragmentCreatExpenseBinding.inflate(inflater, container, false)
+
+        /*val spinner: Spinner = binding.spinnerCategories
+        ArrayAdapter.createFromResource(
+            this, R.array.categories_array, android.R.layout.simple_spinner_dropdown_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears.
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner.
+            spinner.adapter = adapter
+        }
+
+         */
+
+        return binding.root
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        TODO("Not yet implemented")
+    }
+
+}
