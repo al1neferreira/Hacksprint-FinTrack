3ba86539bcdb5ea72e9802bcd171003045fe91e8
Gabriel Zattar <gabriel.zattar2000@gmail.com>
2024-05-17 18:44:01 -0300

    feat: add RecyclerView for transactions.

--- app/src/main/java/com/example/fintrack/home/HomeActivity.kt
+++ app/src/main/java/com/example/fintrack/home/HomeActivity.kt
@@ -1,16 +1,21 @@
-package com.example.fintrack
+package com.example.fintrack.home
 
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fintrack.R
+import com.example.fintrack.adapter.TransactionsAdapter
 import com.example.fintrack.databinding.ActivityHomeBinding
+import com.example.fintrack.model.Transactions
 
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityHomeBinding.inflate(layoutInflater)
@@ -18,8 +23,18 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(view)
 
-        val toolbar: Toolbar =findViewById(R.id.toolbar_home)
+        val toolbar: Toolbar = findViewById(R.id.toolbar_home)
         setSupportActionBar(toolbar)
+
+        recyclerView = findViewById(R.id.rv_transactions)
+
+        val transactions = listOf(
+            Transactions("Market", "Food", "R$50,00", "17-05-2024", "ic_food"),
+            Transactions("Gasoline", "Transportation", "R$30,00", "16-05-2024", "ic_car")
+        )
+
+        val adapter = TransactionsAdapter(transactions)
+        recyclerView.adapter = adapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -33,9 +48,11 @@ class HomeActivity : AppCompatActivity() {
             R.id.menu_more -> {
                 true
             }
+
             R.id.menu_light -> {
                 true
             }
+
             else -> super.onOptionsItemSelected(item)
         }
 
---
+++ app/src/main/java/com/example/fintrack/model/Transactions.kt
@@ -0,0 +1,10 @@
+package com.example.fintrack.model
+
+data class Transactions(
+    val title: String,
+    val category: String,
+    val amount: String,
+    val date: String,
+    val image: String
+) {
+}
